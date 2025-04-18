6fc5ab380287c9da6139bbedf0e89bd8282ae9d5
Michael Abel <mabel@bitmuster.org>
2025-04-18 08:42:02 +0200

    Add test

--- src/element.rs
+++ src/element.rs
@@ -15,3 +15,36 @@ pub struct Element {
     pub children: Vec<Element>,
     pub result: ResultType,
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_new_tree() {
+        let mut suite = Element {
+            et: ElementType::Suite,
+            children: Vec::new(),
+            result: ResultType::None,
+        };
+        let new_test = Element {
+            et: ElementType::Test,
+            children: Vec::new(),
+            result: ResultType::Pass,
+        };
+        suite.children.push(new_test);
+        let new_test = Element {
+            et: ElementType::Test,
+            children: Vec::new(),
+            result: ResultType::Fail,
+        };
+        suite.children.push(new_test);
+        let new_kw = Element {
+            et: ElementType::Keyword,
+            children: Vec::new(),
+            result: ResultType::None,
+        };
+        let mut et: &mut Element = suite.children.get_mut(1).unwrap();
+        et.children.push(new_kw);
+    }
+}
