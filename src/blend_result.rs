6c1da3638bdb959af9406df885eab899aebe600c
Michael Abel <mabel@bitmuster.org>
2025-04-18 11:50:18 +0200

    Fight with borrwo checker

--- src/blend_result.rs
+++ src/blend_result.rs
@@ -3,6 +3,8 @@ use quick_xml::events::attributes::AttrError;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 use std::any;
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::str;
 
 use crate::element::{Element, ElementType, ResultType};
@@ -40,88 +42,104 @@ pub fn parse(xml_file: &str) {
 
     let mut buf = Vec::new();
     let mut depth = 0;
-    let mut root_element: Option<Element> = None;
-    let mut current: Option<&mut Element> = None;
-    loop {
-        let ident = " ".repeat(depth * 4 + 4);
-        match reader.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
+    let root_element: Rc<RefCell<Element>> = Rc::new(RefCell::new(Element {
+        et: ElementType::Robot,
+        children: Vec::new(),
+        result: ResultType::None,
+    }));
+    let binding = root_element.clone();
+    {
+        let mut current: &mut Element = &mut binding.borrow_mut();
+        loop {
+            let ident = " ".repeat(depth * 4 + 4);
+            match reader.read_event_into(&mut buf) {
+                Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
 
-            Ok(Event::Eof) => break,
+                Ok(Event::Eof) => break,
 
-            Ok(Event::Start(e)) => {
-                //println!("  Start {}", any::type_name_of_val(&e));
-                println!(
-                    "{ident}Start: {}",
-                    str::from_utf8(e.local_name().as_ref()).unwrap()
-                );
-                print_attributes(&ident, e.attributes());
+                Ok(Event::Start(e)) => {
+                    //println!("  Start {}", any::type_name_of_val(&e));
+                    println!(
+                        "{ident}Start: {}",
+                        str::from_utf8(e.local_name().as_ref()).unwrap()
+                    );
+                    print_attributes(&ident, e.attributes());
 
-                match e.name().as_ref() {
-                    b"robot" => (),
-                    b"suite" => {
-                        root_element = Some(Element {
-                            et: ElementType::Suite,
-                            children: Vec::new(),
-                            result: ResultType::None,
-                        });
-                        current = Some(root_element.as_mut().unwrap());
+                    match e.name().as_ref() {
+                        b"robot" => (),
+                        b"suite" => {
+                            let suite_element = Element {
+                                et: ElementType::Suite,
+                                children: Vec::new(),
+                                result: ResultType::None,
+                            };
+                            //let mut curr = current.borrow_mut();
+                            current.children.push(suite_element);
+                            //let mut new = *curr.children.last_mut().unwrap();
+                            //*curr = new;
+
+                            //current = Rc::new(RefCell::new(suite_element));
+                        }
+                        b"test" => {
+                            let mut new_element = Element {
+                                et: ElementType::Test,
+                                children: Vec::new(),
+                                result: ResultType::None,
+                            };
+                            println!("{:?}", new_element);
+                            println!("{:?}", current);
+                            //current.as_mut().unwrap().children.push(new_element);
+                            println!("{:?}", current);
+                            println!("Root {:?}", root_element);
+                            //current = Some(current.as_mut().unwrap());
+                        }
+                        b"kw" => (),
+                        b"doc" => (),
+                        b"arg" => (),
+                        b"statistics" => (),
+                        b"total" => (),
+                        b"errors" => (),
+                        b"stat" => (),
+                        b"tag" => (),
+                        s => println!("Unmatched {:?}", str::from_utf8(s).unwrap()),
                     }
-                    b"test" => {
-                        let mut new_element = Element {
-                            et: ElementType::Test,
-                            children: Vec::new(),
-                            result: ResultType::None,
-                        };
-                        current.as_mut().unwrap().children.push(new_element);
-                        //current = Some(&current.unwrap());
-                    }
-                    b"kw" => (),
-                    b"doc" => (),
-                    b"arg" => (),
-                    b"statistics" => (),
-                    b"total" => (),
-                    b"errors" => (),
-                    b"stat" => (),
-                    b"tag" => (),
-                    s => println!("Unmatched {:?}", str::from_utf8(s).unwrap()),
+                    depth += 1;
                 }
-                depth += 1;
-            }
-            Ok(Event::Text(e)) => {
-                //println!("{ident}Text {}", any::type_name_of_val(&e));
-                println!("{ident}Text: {}", e.unescape().unwrap());
-            }
-            Ok(Event::End(e)) => {
-                //println!("  End {}", any::type_name_of_val(&e));
-                depth -= 1;
-                let ident = " ".repeat(depth * 4 + 4);
-                println!(
-                    "{ident}End: {}",
-                    str::from_utf8(e.local_name().as_ref()).unwrap()
-                );
-            }
-            Ok(Event::Empty(e)) => {
-                //println!("{ident}Empty {}", any::type_name_of_val(&e));
-                println!(
-                    "{ident}Empty: {}",
-                    str::from_utf8(e.local_name().as_ref()).unwrap()
-                );
-                print_attributes(&ident, e.attributes());
-            }
-            Ok(Event::Decl(e)) => {
-                println!("{ident}Decl {}", any::type_name_of_val(&e));
-            }
+                Ok(Event::Text(e)) => {
+                    //println!("{ident}Text {}", any::type_name_of_val(&e));
+                    println!("{ident}Text: {}", e.unescape().unwrap());
+                }
+                Ok(Event::End(e)) => {
+                    //println!("  End {}", any::type_name_of_val(&e));
+                    depth -= 1;
+                    let ident = " ".repeat(depth * 4 + 4);
+                    println!(
+                        "{ident}End: {}",
+                        str::from_utf8(e.local_name().as_ref()).unwrap()
+                    );
+                }
+                Ok(Event::Empty(e)) => {
+                    //println!("{ident}Empty {}", any::type_name_of_val(&e));
+                    println!(
+                        "{ident}Empty: {}",
+                        str::from_utf8(e.local_name().as_ref()).unwrap()
+                    );
+                    print_attributes(&ident, e.attributes());
+                }
+                Ok(Event::Decl(e)) => {
+                    println!("{ident}Decl {}", any::type_name_of_val(&e));
+                }
 
-            /*            Ok(x) => {
-                println!("Ok {:?}", any::type_name_of_val(&x));
-            }*/
-            x => {
-                println!("No Type {:?}", any::type_name_of_val(&x));
+                /*            Ok(x) => {
+                    println!("Ok {:?}", any::type_name_of_val(&x));
+                }*/
+                x => {
+                    println!("No Type {:?}", any::type_name_of_val(&x));
+                }
             }
+            buf.clear();
         }
-        buf.clear();
     }
-
-    println!("{:?}", root_element.unwrap());
+    println!("{:?}", root_element.borrow());
+    //println!("{:?}", current);
 }
--- src/element.rs
+++ src/element.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, PartialEq)]
 pub enum ElementType {
+    Robot,
     Suite,
     Test,
     Keyword,
