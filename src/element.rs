60e18869a0a3f093f887f9ca2066945905ef792f
Michael Abel <mabel@bitmuster.org>
2025-04-18 08:55:37 +0200

    Extend tests

--- src/element.rs
+++ src/element.rs
@@ -1,15 +1,19 @@
+
+#[derive(Debug, PartialEq)]
 pub enum ElementType {
     Suite,
     Test,
     Keyword,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum ResultType {
     Pass,
     Fail,
     None,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Element {
     pub et: ElementType,
     pub children: Vec<Element>,
@@ -20,6 +24,16 @@ pub struct Element {
 mod test {
     use super::*;
 
+    #[test]
+    fn new_elements(){
+        let _elem = Element {
+            et: ElementType::Suite,
+            children: Vec::new(),
+            result: ResultType::None,
+        };
+        //assert
+    }
+    
     #[test]
     fn test_new_tree() {
         let mut suite = Element {
@@ -44,7 +58,14 @@ mod test {
             children: Vec::new(),
             result: ResultType::None,
         };
-        let mut et: &mut Element = suite.children.get_mut(1).unwrap();
+        //let mut et: &mut Element = suite.children.get_mut(1).unwrap();
+        let et: &mut Element = suite.children.get_mut(1).unwrap();
         et.children.push(new_kw);
+        assert_eq!( suite.children.get(0).unwrap().et, ElementType::Test);
+        assert_eq!( suite.children.get(0).unwrap().result, ResultType::Pass);
+        assert_eq!( suite.children.get(1).unwrap().et, ElementType::Test);
+        assert_eq!( suite.children.get(1).unwrap().result, ResultType::Fail);
+        assert_eq!( suite.children.get(1).unwrap().children.get(0).unwrap().et, ElementType::Keyword);
+        assert_eq!( suite.children.get(1).unwrap().children.get(0).unwrap().result, ResultType::None);
     }
 }
