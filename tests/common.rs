66533972680d437797f5b429ccf674c36a5febb0
Michael Abel <mabel@bitmuster.org>
2025-04-23 19:15:55 +0200

    Process erros if venv is missing

--- tests/common.rs
+++ tests/common.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use anyhow::Context;
 use std::process;
 
@@ -6,7 +7,7 @@ pub fn run_rf_test(t: &str) -> anyhow::Result<()> {
         .arg("-c")
         .arg("rm -f output_{t}.xml log_{t}.html report_{t}.html")
         .output()
-        .context("failed to execute process")?;
+        .context("failed to execute process : rm")?;
     println!(
         "Process Outputs:\n{}",
         String::from_utf8(output.stdout).context("Utf8 conversion failed")?
@@ -15,10 +16,18 @@ pub fn run_rf_test(t: &str) -> anyhow::Result<()> {
         .arg("-c")
         .arg(
             format!(
-            ". venv/bin/activate; robot -d robot/results/ -o output_{t}.xml -l log_{t}.html -r report_{t}.html robot/test_{t}.robot")
+            ". venv/bin/activate && robot -d robot/results/ -o output_{t}.xml -l log_{t}.html -r report_{t}.html robot/test_{t}.robot")
         )
         .output()
-        .context("failed to execute process")?;
+        .context("Failed to execute process: robotframework")?;
+    if !output.status.success() {
+        println!(
+            "Robot Error:\n{:?}",
+            String::from_utf8(output.stderr).context("Utf8 conversion failed")?
+        );
+        return Err(anyhow!(String::from("Something")));
+    };
+
     println!(
         "Robot Outputs:\n{}",
         String::from_utf8(output.stdout).context("Utf8 conversion failed")?
