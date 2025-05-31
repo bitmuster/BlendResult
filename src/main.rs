7576309dfdd4a0f01943a2595a0f85eaf66c7b03
Michael Abel <mabel@bitmuster.org>
2025-05-31 20:23:53 +0200

    Add clap parser

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,41 @@ mod element;
 mod multi_result_list;
 mod rf_parser;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Parse {
+        filename: Option<String>,
+        csv_file: Option<String>,
+    },
+    Blend {
+        name: Option<String>,
+    },
+}
+
 fn main() -> anyhow::Result<()> {
     simple_logger::SimpleLogger::new().env().init().unwrap();
 
-    let filename = env::args()
-        .nth(1)
-        .context("Wrong amount of command line parameters")?;
-    let csv_file = env::args()
-        .nth(2)
-        .context("Wrong amount of command line parameters")?;
-    println!("Analyzing {}", filename);
-    let xml = fs::read_to_string(filename).context("Reading failed")?;
-    rf_parser::parse(&xml, &csv_file)?;
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Parse { filename, csv_file } => {
+            println!("Parsing {}", filename.as_ref().unwrap());
+            let xml = fs::read_to_string(filename.as_ref().unwrap()).context("Reading failed")?;
+            rf_parser::parse(&xml, &csv_file.as_ref().unwrap())?;
+        }
+        Commands::Blend { name } => {
+            println!("'myapp add' was used, name is: {name:?}");
+        }
+    }
     Ok(())
 }
